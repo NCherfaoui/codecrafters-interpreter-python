4c36e3b0047cc9759fab38f3ed46e4ea44b24dc7
Nassim CHERFAOUI <99206668+NCherfaoui@users.noreply.github.com>
2024-08-24 00:38:13 +0200

    pass stage

--- app/main.py
+++ app/main.py
@@ -49,7 +49,7 @@ def main():
         if c == "*":
             print("STAR * null")
         else:
-            print(f"Error: Unexpected character: {c}")
+            print(f"[LINE {line_number}] Error: Unexpected character: {c}")
     print("EOF  null")
 if __name__ == "__main__":
     main()
