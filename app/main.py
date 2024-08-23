cc488c8cb7d56643a5267a16fa2874755ad33630
Nassim CHERFAOUI <99206668+NCherfaoui@users.noreply.github.com>
2024-08-24 00:20:21 +0200

    pass stage

--- app/main.py
+++ app/main.py
@@ -20,11 +20,16 @@ def main():
         file_contents = file.read()
 
     # Uncomment this block to pass the first stage
-    if file_contents:
-        raise NotImplementedError("Scanner not implemented")
-    else:
-        print("EOF  null") # Placeholder, remove this line when implementing the scanner
+    # if file_contents:
+    #     raise NotImplementedError("Scanner not implemented")
+    # else:
+    #     print("EOF  null") # Placeholder, remove this line when implementing the scanner
 
-
+    for c in file_contents:
+        if c == "(":
+            print("LEFT_PAREN ( null")
+        if c == ")":
+            print("RIGHT_PAREN ) null")
+    print("EOF  null")
 if __name__ == "__main__":
     main()
